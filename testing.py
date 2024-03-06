52313d9d103847096d31153b32296cbb5c082dac
Yuvraj Jain <yjain@unc.edu>
2024-03-06 00:06:53 -0500

    Add demos

--- testing.py
+++ testing.py
@@ -1,1 +1,11 @@
-"Test File"
+"""Testing file: Contains playground"""
+
+from transformers import pipeline
+
+classifier = pipeline(
+    task="sentiment-analysis",
+    )
+
+res = classifier("Depresso Espresso")
+
+print(res)
